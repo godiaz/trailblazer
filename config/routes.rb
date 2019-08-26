90cbd2a64aef4f13a0f4082b31668a583ccbb377
Gonzalo Diaz <info@ventra.com.co>
2019-08-26 17:00:19 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get '/privacy', to: 'pages#privacy'
+  get '/about', to: 'pages#about'
+  get '/preparation', to: 'pages#preparation'
+  get '/contact', to: 'pages#contact'
+  get '/safaris', to: 'pages#safaris'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
