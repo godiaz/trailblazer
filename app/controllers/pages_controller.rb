f0a2914de2f1e4d6a14b8d5b45e30d5bb171497f
Gonzalo Diaz <info@ventra.com.co>
2019-08-27 19:37:57 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contact
+    @markers =
+      {
+        lat: 10.986914,
+        lng: -74.779365
+      },
+      {
+        lat: 10.423356,
+        lng: -75.548130
+      },
+      {
+        lat: 4.661928,
+        lng: -74.109624
+      }
+  end
 end
