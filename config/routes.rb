5f2732239d2f7d4aa338064150ce1232bdae7cd2
Gonzalo Diaz <info@ventra.com.co>
2019-09-02 10:23:30 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/preparation', to: 'pages#preparation'
   get '/contact', to: 'pages#contact'
   get '/safaris', to: 'pages#safaris'
+  get '/reviews', to: 'pages#reviews'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
