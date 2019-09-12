b29d3af5b912a88818847d87404583fa76608d0d
Gonzalo Diaz <info@ventra.com.co>
2019-09-12 15:50:23 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :privacy, :about, :contact, :reviews]
+  skip_before_action :authenticate_user!, only: [:home, :privacy, :about, :contact, :reviews, :preparation]
 
   def home
     @safaris = Safari.all
