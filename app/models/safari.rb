7c61808280611d5f4cbfaa93fe7bf9bf7bcec430
Gonzalo Diaz <info@ventra.com.co>
2019-12-12 13:19:20 +0000

    done

--- app/models/safari.rb
+++ app/models/safari.rb
@@ -1,5 +1,5 @@
 class Safari < ApplicationRecord
-  has_many :safari_attraction
+  has_many :safari_attractions, dependent: :destroy
   has_many :attractions, through: :safari_attraction
   mount_uploader :banner_image, PhotoUploader
   belongs_to :user
